e2530f315bfe4d636b3aefdfae223a9bdb60a253
WinsomeYang <kkadso@gmail.com>
2016-11-08 18:02:54 +0800

    計算總共有幾個comment

--- src/comments/api/serializers.py
+++ src/comments/api/serializers.py
@@ -8,6 +8,7 @@ from comments.models import Comment
 
 
 class CommentSerializer(ModelSerializer):
+    reply_count = SerializerMethodField()
     class Meta:
         model = Comment
         fields = [
@@ -16,8 +17,14 @@ class CommentSerializer(ModelSerializer):
             'object_id',
             'parent',
             'content',
+            'reply_count',
+            'timestamp'
         ]
 
+    def get_reply_count(self, obj):
+        if obj.is_parent:
+            return obj.children().count()
+        return 0
 
 class CommentChildSerializer(ModelSerializer):
     class Meta:
@@ -30,6 +37,7 @@ class CommentChildSerializer(ModelSerializer):
 
 class CommentDetailSerializer(ModelSerializer):
     replies = SerializerMethodField()
+    reply_count = SerializerMethodField()
     class Meta:
         model = Comment
         fields = [
@@ -39,9 +47,15 @@ class CommentDetailSerializer(ModelSerializer):
             'content',
             'replies',
             'timestamp',
+            'reply_count',
         ]
 
     def get_replies(self, obj):
         if obj.is_parent:
             return CommentChildSerializer(obj.children(), many=True).data
         return None
+
+    def get_reply_count(self, obj):
+        if obj.is_parent:
+            return obj.children().count()
+        return 0
