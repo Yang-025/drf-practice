5b9485dbc0c88cb1a000a19b69a46e1216b059b8
WinsomeYang <kkadso@gmail.com>
2016-11-08 10:44:00 +0800

    把List和Detail分成兩個serializer

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -2,16 +2,16 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 
 from posts.models import Post
-from .serializers import PostSerializer
-
-
-class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
+from .serializers import PostDetailSerializer, PostListSerializer
 
 class PostDetailAPIView(RetrieveAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostDetailSerializer
     lookup_field = 'slug'
     lookup_url_kwarg = 'abc'
+
+class PostListAPIView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+
+
