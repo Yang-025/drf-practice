6fa88e70445afd18b9dbfd7cfe56550239ff879f
WinsomeYang <kkadso@gmail.com>
2016-11-08 09:47:18 +0800

    4. Serialize Model Data into JSON

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.generics import ListAPIView
 
 
 from posts.models import Post
+from .serializers import PostSerializer
 
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
+    serializer_class = PostSerializer
