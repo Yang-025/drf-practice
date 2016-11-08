a421017f58d03958ebe74abc93d12bef3cc991bc
WinsomeYang <kkadso@gmail.com>
2016-11-08 14:19:05 +0800

    可以輸入查詢字串

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from rest_framework.generics import (
     ListAPIView,
     RetrieveAPIView,
@@ -55,7 +57,21 @@ class PostDeleteAPIView(DestroyAPIView):
     # lookup_url_kwarg = 'abc'
 
 class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
+    # queryset = Post.objects.all()
     serializer_class = PostListSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        # queryset_list = super(PostListAPIView, self).get_queryset(*args,**kwargs)
+        queryset_list = Post.objects.all()
+        query = self.request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+            ).distinct()
+
+        return queryset_list
 
 
