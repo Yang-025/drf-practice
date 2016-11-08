f811aa0d4adabd361ce7fe4f88f6156493bd3e1a
WinsomeYang <kkadso@gmail.com>
2016-11-08 14:00:13 +0800

    客製化的permission

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -7,8 +7,16 @@ from rest_framework.generics import (
     RetrieveUpdateAPIView,
 )
 
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly
+)
+
 
 from posts.models import Post
+from .permissions import IsOwnerOrReadOnly
 from .serializers import (
     PostCreateUpdateSerializer,
     PostDetailSerializer,
@@ -19,6 +27,7 @@ from .serializers import (
 class PostCreateAPIView(CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateUpdateSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -34,7 +43,7 @@ class PostUpdateAPIView(RetrieveUpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateUpdateSerializer
     lookup_field = 'slug'
-    # lookup_url_kwarg = 'abc'
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
