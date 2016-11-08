40e225ab4d62f27aee1c216488b35b6a90160ecd
WinsomeYang <kkadso@gmail.com>
2016-11-08 11:03:18 +0800

    增加 PUT ,DELETE 方法

--- src/posts/api/urls.py
+++ src/posts/api/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 
 from .views import (
 	PostDetailAPIView,
-    PostListAPIView
+    PostListAPIView,
+	PostUpdateAPIView,
+	PostDeleteAPIView
+
 	)
 
 urlpatterns = [
@@ -11,8 +14,7 @@ urlpatterns = [
     # url(r'^create/$', post_create),
     # pk:primary key \d+ : digit regex
     # url(r'^(?P<pk>\d+)/$', PostDetailAPIView.as_view(), name='detail'),
-    url(r'^(?P<abc>[\w-]+)/$', PostDetailAPIView.as_view(), name='detail'),
-    # url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
-    # url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
-    # #url(r'^posts/$', "<appname>.views.<function_name>"),
+    url(r'^(?P<slug>[\w-]+)/$', PostDetailAPIView.as_view(), name='detail'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', PostUpdateAPIView.as_view(), name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', PostDeleteAPIView.as_view(), name='delete'),
 ]
--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -1,4 +1,9 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+)
 
 
 from posts.models import Post
@@ -8,7 +13,19 @@ class PostDetailAPIView(RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
     lookup_field = 'slug'
-    lookup_url_kwarg = 'abc'
+    # lookup_url_kwarg = 'abc'
+
+class PostUpdateAPIView(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
+    # lookup_url_kwarg = 'abc'
+
+class PostDeleteAPIView(DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
+    # lookup_url_kwarg = 'abc'
 
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
