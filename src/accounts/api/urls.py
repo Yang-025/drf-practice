75e29109c5a5e6b2c1e80b62358f62a5b800d1dc
WinsomeYang <kkadso@gmail.com>
2016-11-09 16:04:25 +0800

    28 of 33 - Base APIView for User Login

--- src/accounts/api/urls.py
+++ src/accounts/api/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 
 from .views import (
     UserCreateAPIView,
+    UserLoginAPIView,
 
     )
 
 urlpatterns = [
+    url(r'^login/$', UserLoginAPIView.as_view(), name='login'),
     url(r'^register/$', UserCreateAPIView.as_view(), name='register'),
 ]
